819fb112617a131cd2ac8a813ca5ea46efbbf28c
Zig Zichterman <github@ziggr.com>
2018-04-11 14:47:28 -0700

    Working towards a working emitter
    Starting to emit output lines.
    The math is all wrong, but at least we're emitting.

--- ZZCraftoriumLayout_Parser.py
+++ ZZCraftoriumLayout_Parser.py
@@ -2,9 +2,10 @@
 #
 # python3 parser <input.txt> <output.lua>
 #
-import sys
-import re
 import collections
+import math
+import re
+import sys
 
 # Write a message to log, or stdout, or whatever.
 # Or don't.
@@ -39,10 +40,10 @@ def parse_constant_assignment(line, parsed_line):
     val = m.group(2)
     val = val.replace(',', '')
     val = int(val)
-    parsed_line["assign"] = { var: var
-                            , val: val
+    parsed_line["assign"] = { "var": var
+                            , "val": val
                             }
-    LOG("assign: {} = {}", var, val)
+    LOG("parse assign: {} = {}", var, val)
     return parsed_line
 
 # X3 Z14 Y19,000
@@ -122,14 +123,14 @@ def parse_one_line(line, line_number, input_filepath):
                   , 'line'        : line
                   , 'file'        : input_filepath
                   }
-
+    LOG("parse {}:{}  {}".format(input_filepath, line_number, line.strip()))
                         # Strip, but remember, comments
     l = line.strip()
     m = re.search(r'#.*', l)
     if m:
         parsed_line["comment"] = m.group(0)
         l = l[:m.start()].strip()
-        LOG("retain: {}  comment: {}", l, parsed_line["comment"])
+        # LOG("retain: {}  comment: {}", l, parsed_line["comment"])
 
                         # Skip blank lines (or blank after comments removed)
     if not l:
@@ -147,21 +148,21 @@ def parse_one_line(line, line_number, input_filepath):
     station_ct = parse_station_ct(line)
     if station_ct:
         parsed_line["station_ct"] = station_ct
-        LOG("station_ct:{}",station_ct)
+        LOG("parse station_ct:{}",station_ct)
         return parsed_line
 
                         # NESW direction
     direction = parse_direction(line)
     if direction:
         parsed_line['direction'] = direction
-        LOG("direction: {}", direction)
+        LOG("parse direction: {}", direction)
         return parsed_line
 
                         # lamp control
     lamp = parse_lamp(line)
     if lamp:
         parsed_line['lamp'] = lamp
-        LOG("lamp: {}", lamp)
+        LOG("parse lamp: {}", lamp)
         return parsed_line
 
                         # individual stations with optional XZY coords
@@ -170,7 +171,7 @@ def parse_one_line(line, line_number, input_filepath):
         x = parsed_line.get('x')
         z = parsed_line.get('z')
         y = parsed_line.get('y')
-        LOG("station:{station}  X:{x} Z:{z} Y:{y}"
+        LOG("parse station xzy:{station}  X:{x} Z:{z} Y:{y}"
              , station=station, x=x, y=y, z=z)
         return parsed_line
 
@@ -180,7 +181,7 @@ def parse_one_line(line, line_number, input_filepath):
         x = parsed_line.get('x')
         z = parsed_line.get('z')
         y = parsed_line.get('y')
-        LOG("X:{x} Z:{z} Y:{y}", x=x, y=y, z=z)
+        LOG("parse xzy X:{x} Z:{z} Y:{y}", x=x, y=y, z=z)
         return parsed_line
 
 
@@ -200,16 +201,22 @@ def ParserState():
     return parser_state
 
 def apply_line(parsed_line, parser_state):
-
+    LOG("apply {}:{:<3} {}".format( parsed_line["file"]
+                                   , parsed_line["line_number"]
+                                   , parsed_line["line"].strip()
+                                   ))
                         # furnishing assignment
     furn = parsed_line.get('furn')
     if furn:
-        parser_state['furn'][furn.station].append(furn.id)
+        parser_state['furn'][furn.get('station')].append(furn.get('id'))
         return
                         # variable assignment
     assign = parsed_line.get('assign')
     if assign:
-        parser_state['constant'][assign.var] = assign.val
+        var = assign["var"]
+        val = assign["val"]
+        LOG("apply assign: {} = {}".format(var, val))
+        parser_state['constant'][var] = val
         return
 
                         # single-station manual location
@@ -273,10 +280,12 @@ def apply_line(parsed_line, parser_state):
 
 def error_if_queue_not_empty(parsed_line, parser_state):
     if len(parser_state['item_queue']) <= 0:
-        sys.stderr.write("### Error {} line {}: item queue not empty"
-                         .format( parsed_line['file']
-                                , parsed_line['line_number']
-                                ))
+        s = ("### Error {} line {}: item queue not empty"
+             .format( parsed_line['file']
+                    , parsed_line['line_number']
+                    ))
+        LOG(s)
+        sys.stderr.write(s)
         sys.exit(1)
 
 # Rotation, in degrees, for a station when running along
@@ -299,6 +308,7 @@ WIDTH = {
     , 'ww'        : 200
 }
 def emit_station(parsed_line, parser_state):
+    LOG("Emit station {}".format(parsed_line.get(station)))
     # write the single station specified by parsed_line, with
     # any missing info (usually rotation, maybey Y coord) from parser state
     station  = parsed_line['station']
@@ -315,6 +325,8 @@ def emit_station(parsed_line, parser_state):
     emit_line(args, parser_state)
 
 def emit_queue(parsed_line, parser_state):
+    LOG("Emit queue: {}".format(" ".join(parser_state.get('item_queue'))))
+
     # Evenly distribute the distance between parsed_line's coordinates (the end)
     # and parser_state's coordinates (the beginning) across all of the
     # lamps and stations in the item_queue.
@@ -335,39 +347,41 @@ def emit_queue(parsed_line, parser_state):
                         # station, since we want it at the end coord
                         # on the current parser_line.
     total_item_width = 0
-    for item in parser_state['queue'][1:-1]:
+    for item in parser_state['item_queue'][1:-1]:
         total_item_width += WIDTH[item]
 
                         # How much wall space must we stretch across?
-    total_wall_width = math.sqrt( delta_total.get("x")**2 + delta_total.get("z") )
+    total_wall_width = math.sqrt( delta_total.get("x")**2
+                                + delta_total.get("z")**2 )
 
     curr_coord = { "x" : start_coord.get("x")
                  , "z" : start_coord.get("z")
                  , "y" : start_coord.get("y")
                  }
     cume_width = 0
-    for item in parser_state['queue']:
+    for item in parser_state['item_queue']:
                         # Emit this station.
         args = { "station"       : item
                , "station_index" : next_station_index(item, parser_state)
                , "x"             : curr_coord.get("x")
                , "z"             : curr_coord.get("z")
                , "y"             : curr_coord.get("y")
-               , "rotation"      : ROTATION[parser_state.get("rotation")] }
+               , "rotation"      : ROTATION[parser_state.get("direction")] }
         emit_line(args, parser_state)
 
                         # Move to next position.
         cume_width += WIDTH[item]
         ratio      = cume_width / total_item_width * total_wall_width
         for axis in ["x","z","y"]:
-            cuur_coord[axis] = ratio * delta_total[axis]
+            curr_coord[axis] = ratio * delta_total[axis]
 
 def emit_line(args, parser_state):
-    quoted_station = '"{station} {station_index}"'.format(*args)
+    quoted_station = '"{station} {station_index}"'.format(**args)
     args["quoted_station"] = quoted_station
     template = '[{quoted_station:<10}] = "{x:>5.0f}\t{z:>5.0f}\t{y:>5.0f}\t{rotation:3}"'
-    line     = template.format(*args)
-    parser_state.output_lines.append(line)
+    line     = template.format(**args)
+    LOG("EMIT {}".format(line))
+    parser_state["output_lines"].append(line)
 
 def next_station_index(station, parser_state):
     index   = 1 + (parser_state['furn_index_prev'].get(station) or 1)
@@ -387,8 +401,9 @@ def is_lamp(item):
 
 def parse_lines(input_lines, input_filepath, parser_state):
     output_lines = []
-    line_number  = 1
+    line_number  = 0
     for line in input_lines:
+        line_number += 1
         parsed_line = parse_one_line(line, line_number, input_filepath)
         apply_line(parsed_line, parser_state)
     return output_lines
@@ -397,24 +412,21 @@ def parse_lines(input_lines, input_filepath, parser_state):
 # -- main --------------------------------------------------------------------
 
 def main():
-    print("Cheers luv!")
+    LOG("Cheers luv!")
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: python3 Parser.py <inputfile> <outputfile>")
         sys.exit(1)
 
-
-    input_filepath  = sys.argv[1]
     output_filepath = sys.argv[-1]
     parser_state    = ParserState()
-
-    output_lines = []
-    for input_filepath in sys.argv[1:-2]:
+    for input_filepath in sys.argv[1:-1]:
+        LOG("file: {}".format(input_filepath))
         with open(input_filepath) as f:
             input_lines = f.readlines()
 
-        ol = parse_lines(input_lines, input_filepath, parser_state)
-        output_lines.extend(ol)
+        parse_lines(input_lines, input_filepath, parser_state)
 
+    output_lines = parser_state.get('output_lines')
     with open(output_filepath, "w") as f:
         f.write("\n".join(output_lines))
 
